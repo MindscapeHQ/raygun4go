58fa730713c475106beeba04edac31fbc1b69078
Alik Hochner <alik.hochner@gmail.com>
2016-05-23 13:36:09 +0300

    updated packages

--- raygun4go.go
+++ raygun4go.go
@@ -45,7 +45,7 @@ import (
 	"log"
 	"net/http"
 
-	"code.google.com/p/go-uuid/uuid"
+	"github.com/pborman/uuid"
 	gcontext "golang.org/x/net/context" //google context
 	"google.golang.org/appengine/urlfetch"
 )
